2cb74343d17acfcf0956979b713f5217f9a8c7ea
soomin Kang <tnals3256@naver.com>
2024-04-24 18:58:23 +0900

    로그인

--- back/src/main/java/com/shop/back/LoginTestController.java
+++ back/src/main/java/com/shop/back/LoginTestController.java
@@ -1,10 +1,10 @@
-package com.shop.back;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+//package com.shop.back;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
 //@Controller
 //@RestController
 //public class LoginTestController {
